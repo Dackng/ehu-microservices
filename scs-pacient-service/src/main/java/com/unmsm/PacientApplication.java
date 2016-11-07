cbb66b6ccd7ec466a2e64bdcc3d3c0ccec4de7b6
Dackng <dackng@gmail.com>
2016-11-06 21:14:24 -0500

    pacientapplication

--- scs-pacient-service/src/main/java/com/unmsm/PacientApplication.java
+++ scs-pacient-service/src/main/java/com/unmsm/PacientApplication.java
@@ -2,12 +2,22 @@ package com.unmsm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class PacientApplication {
 
 	public static void main(String[] args) {
