252be2f8ece9090d8cc854b98545201942283ddc
Dackng <dackng@gmail.com>
2017-06-18 19:10:33 -0500

    removing catalog fields

--- ehu-patient-service/src/main/java/com/unmsm/api/PatientService.java
+++ ehu-patient-service/src/main/java/com/unmsm/api/PatientService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.unmsm.catalog.Catalog;
 import com.unmsm.patient.Patient;
 import com.unmsm.patient.PatientRepository;
 import com.unmsm.ubigeo.Ubigeo;
@@ -30,12 +29,6 @@ public class PatientService {
 	public Patient findPatientDetailByCode(Integer code) {
 		Patient patient = patientRepository.findPatientByCode(code);
 		if(patient != null){
-			Catalog catalog = restTemplate.getForObject(
-					"http://ehu-catalog-service/api/element/civil-state/" + patient.getCivilStateId(),Catalog.class);
-			patient.setCivilStateName(catalog.getName());
-			catalog = restTemplate.getForObject(
-					"http://ehu-catalog-service/api/element/eap/" + patient.getEapId(),Catalog.class);
-			patient.setEapName(catalog.getName());
 			Ubigeo ubigeo = restTemplate.getForObject(
 					"http://ehu-ubigeo-service/api/element/ubigeo/" +
 					patient.getUbigeo().getUbigeoCode(), Ubigeo.class);
